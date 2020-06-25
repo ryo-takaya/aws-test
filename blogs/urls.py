80c5d553cb2850d359e862ceb73496141ac1a18f
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-06-25 11:29:34 +0900

    initial commit

---
+++ blogs/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+ 
+# set the application namespace
+# https://docs.djangoproject.com/ja/2.0/intro/tutorial03/
+app_name = 'blogs'
+ 
+urlpatterns = [
+    # 一覧表示のURL: /
+    path('', views.IndexView.as_view(), name='index'),  # url is /
+ 
+    # 新規作成のURL: /post/create/
+    path('post/create/', views.CreateView.as_view(), name='create'),
+ 
+    # 以下では記事IDを 123 とします
+    # 詳細表示のURL: /post/123/
+    path('post/<int:pk>/', views.DetailView.as_view(), name='detail'),
+ 
+    # 内容更新のURL: /post/123/update/
+    path('post/<int:pk>/update/', views.UpdateView.as_view(), name='update'),
+    
+    # 記事削除のURL: /post/123/delete/
+    path('post/<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+]
---
+++ blogs/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Post
+ 
+ 
+class IndexView(generic.ListView): #  generic.ListViewを継承
+    model = Post # 使用するモデル
+    paginate_by = 5 # 1ページあたりの表示件数をカスタマイズ（学習のため）
+    ordering = ['-updated_at'] # 並び順を更新時刻が新しい順にカスタマイズ（学習のため）
+    template_name = 'blogs/index.html' # 表示に使用するテンプレート（この後すぐ作成）
+ 
+ 
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blogs/detail.html'
+ 
+ 
+class CreateView(generic.edit.CreateView):
+    model = Post
+    fields = '__all__'  # ['title', 'text', 'author']
+    template_name = 'blogs/create.html'
+ 
+ 
+class UpdateView(generic.edit.UpdateView):
+    model = Post
+    fields = '__all__'  # ['title', 'text', 'author']
+    template_name = 'blogs/update.html'
+ 
+ 
+class DeleteView(generic.edit.DeleteView):
+    model = Post
+    success_url = reverse_lazy('blogs:index') # 削除完了時に遷移するページを指定
+    template_name = 'blogs/delete.html'
